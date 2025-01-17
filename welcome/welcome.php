b4c23b7db302fe503cfec431b453503295c812d1
15693 <zachary.stevens@vtsd.com>
2025-01-17 09:22:25 -0500

    Security Update

--- welcome/welcome.php
+++ welcome/welcome.php
@@ -1,8 +1,26 @@
-<?php include 'header.php'; ?>
+<?php include 'header.php';
+// Setup our variables and set them to empty
+$name = $email = "";
+//Condition to detect form data to sanitize it
+if($_SERVER["REQUEST_METHOD"] == 'POST'){
+    $name =  sanitize_inputs($_POST["name"]);
+    $email =  sanitize_inputs($_POST["email"]);
+}
+
+// Function to sanitize the data
+function sanitize_inputs($data){
+    // Trim method removes spaces at beginning and end
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
 
 <div class="row">
     <div class="col">
-        <p>Welcome <?php echo ($_POST["name"]);?>, your email address is <?php echo ($_POST["email"]);?>.</p>
+        <p>Welcome <?php echo $name;?>,
+            your email address is <?php echo $email;?>.</p>
     </div>
 </div>
 
